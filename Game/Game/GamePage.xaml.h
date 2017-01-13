825fd47cb86c9e29b4ef796d724fd404739a8615
astirbat <alin.stirbats@gmail.com>
2017-01-13 17:38:32 +0200

    basic structure

---
+++ Game/Game/GamePage.xaml.h
@@ -0,0 +1,21 @@
+﻿//
+// GamePage.xaml.h
+// Declaration of the GamePage class
+//
+
+#pragma once
+
+#include "GamePage.g.h"
+
+namespace Game
+{
+	/// <summary>
+	/// An empty page that can be used on its own or navigated to within a Frame.
+	/// </summary>
+	[Windows::Foundation::Metadata::WebHostHidden]
+	public ref class GamePage sealed
+	{
+	public:
+		GamePage();
+	};
+}
